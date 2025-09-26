630130ef38cf6f4bef2dbe70b18359e4e0849fa9
aparrott0024-lab <aparrott0024@pointloma.edu>
2025-09-25 20:02:03 -0700

    Update FizzBuzz.py
    Added comments to the code.

--- FizzBuzz.py
+++ FizzBuzz.py
@@ -2,16 +2,23 @@
 # 9-25-25
 # Fizz Buzz Extra Credit Attempt
 
-# This function counts to 100
+# This function counts to 100.
 def count_to_100():
     for i in range(1, 101):
+        # Checks if i is a multiple of 3 AND 5.
         if (i % 3) == 0 and (i % 5) == 0:
+            # If so, it prints FizzBuzz in its place.
             print ("FizzBuzz")
+        # Checks if i is a multiple of only 5.
         elif (i % 5) == 0:
+            # If so, it prints Buzz in its place.
             print("Buzz")
+        # Checks if i is a multiple of 3.
         elif (i % 3) == 0:
+            # If so, it prints Fizz in its place.
             print("Fizz")
         else:
+            # Prints the number if none of these match.
             print(i)
 
 count_to_100()
